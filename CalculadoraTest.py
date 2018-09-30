8f3e0b7197f562d1c8d4676ae7c83951e6241e0d
ja.martinezl <ja.martinezl@uniandes.edu.co>
2018-09-29 21:05:32 -0500

    Paso 5: Se crea prueba para hacer suma de varios numeros (Rojos)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -16,3 +16,6 @@ class CalculadoraTest(TestCase):
     def test_sumar_cadenaConDosNumeros(self):
         self.assertEqual(Calculadora.sumar("1,3"),4, "Dos numero")
 
+    def test_sumar_cadenaConMultipesNumeros(self):
+        self.assertEqual(Calculadora.sumar("5,2,4,1"), 12, "Dos numero")
+
